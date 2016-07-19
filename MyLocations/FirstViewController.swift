7d0c8369bce6a0833cffdea8b2cb3c12711ad04c
Matthijs <matthijs@Matthijss-MacBook-Pro.local>
2016-07-19 09:06:47 +0200

    Initial Commit

---
+++ MyLocations/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Matthijs on 19/07/2016.
+//  Copyright © 2016 Razeware. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
